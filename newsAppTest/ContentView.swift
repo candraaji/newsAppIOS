44fd1fa4cf0d69a4b9b1990b7246c03c02b26d3a
Candra Ristiawan <Candra.Ristiawan@aia.com>
2022-02-12 19:37:15 +0700

    Initial Commit

---
+++ newsAppTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  newsAppTest
+//
+//  Created by Candra Ristiawan on 12/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ newsAppTest/newsAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsAppTestApp.swift
+//  newsAppTest
+//
+//  Created by Candra Ristiawan on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct newsAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
